b7d1fb11654c31b9cccfe92e811f7a889a404916
kinhthanh <letruongkinhthanh@gmail.com>
2022-04-24 15:38:16 +0700

    random student

--- classHeader.h
+++ classHeader.h
@@ -77,7 +77,78 @@ public:
 
     Date(int day, int month, int year) : day(day), month(month), year(year) {};
     Date() : day(1), month(1), year(1990) {};
+
+    
 };
+
+class RandomIntegerGenerator {
+public:
+    RandomIntegerGenerator();
+public:
+    int next();
+    int next(int);
+    int next(int, int);
+};
+
+RandomIntegerGenerator::RandomIntegerGenerator() {
+    srand(time(NULL));
+}
+
+int RandomIntegerGenerator::next() {
+    int result = rand();
+    return result;
+}
+
+int RandomIntegerGenerator::next(int ceiling) {
+    int result = rand() % ceiling;
+    return result;
+}
+
+int RandomIntegerGenerator::next(int left, int right) {
+    int result = rand() % (left - right + 1) + left;
+    return result;
+}
+
+class RandomDateListGenerator
+{
+private:
+    vector<string> _days;
+    vector<string> _months;
+    vector<string> _years;
+    RandomIntegerGenerator _rdg;
+
+public:
+    
+
+    Date next();
+};
+
+Date RandomDateListGenerator::next() {
+
+    int month = _rdg.next(1, 12);
+
+    int day = 31;
+
+
+
+    if (month == 4 || month == 6 || month == 9 || month == 11)
+    {
+        day = _rdg.next(1, 30);
+    }
+
+    if (month == 2)
+    {
+        while (day == 31 || day == 30) {
+            day = _rdg.next(1, 28);
+        }
+    }
+
+
+    int year = _rdg.next(1990,2022);
+
+    Date result(day, month, year);
+    return result;
+}
 
 class Address
 {
@@ -109,6 +180,65 @@ public:
     Address(int house_number, std::string street, std::string ward, std::string district)
         : house_number(house_number), street(street), ward(ward), district(district) {};
 };
+
+class RandomAddressListGenerator
+{
+private:
+    vector<int> house_number;
+    vector<string> street;
+    vector<string> ward;
+    vector<string> district;
+
+    RandomIntegerGenerator _rdg;
+
+public:
+    RandomAddressListGenerator();
+
+    Address next();
+};
+
+RandomAddressListGenerator::RandomAddressListGenerator()
+{
+    house_number = {
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
+    };
+    street = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+    ward = { "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022" };
+    district = {};
+}
+
+Address RandomAddressListGenerator::next() {
+
+    int index = _rdg.next(_months.size());
+    string month = _months[index];
+
+    string day = "31";
+
+
+
+    if (month == "4" || month == "6" || month == "9" || month == "11")
+    {
+        while (day == "31") {
+            index = _rdg.next(_days.size());
+            day = _days[index];
+        }
+    }
+
+    if (month == "2")
+    {
+        while (day == "31" || day == "30") {
+            index = _rdg.next(_days.size());
+            day = _days[index];
+        }
+    }
+
+
+    index = _rdg.next(_years.size());
+    string year = _years[index];
+
+    DOB result(day, month, year);
+    return result;
+}
 
 class Student
 {
